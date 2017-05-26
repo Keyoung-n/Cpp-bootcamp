edc48d5d7b5ebe5024817d2927bd1e0ee8743dac
keyoung <keang.nage.3@hotmail.com>
2017-05-26 12:22:09 +0200

    auto push

---
+++ d04/ex00/Sorcerer.hpp
@@ -0,0 +1,23 @@
+#ifndef Sorcerer_HPP
+#define Sorcerer_HPP
+#include <iostream>
+
+class Sorcerer {
+private:
+  std::string name;
+  std::string title;
+  Sorcerer();
+
+public:
+  Sorcerer (std::string n, std::string t);
+  ~Sorcerer ();
+  Sorcerer ( Sorcerer const & copy );
+  Sorcerer & operator=( Sorcerer const & copy );
+  std::string getName();
+  std::string getTitle();
+  void setName(std::string n);
+  void setTitle(std::string t);
+};
+
+std::ostream & operator<<(std::ostream & o, Sorcerer const & rhs);
+#endif
